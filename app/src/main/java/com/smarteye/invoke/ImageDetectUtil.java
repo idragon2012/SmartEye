1a7f939a8b23eceba67d8759d64f61cc636d0cc2
68300051 <liuly2@shanghai-electric.com>
2023-05-23 13:32:20 +0800

    feat: use Baidu detect and ChatGPT

---
+++ app/src/main/java/com/smarteye/invoke/ImageDetectUtil.java
@@ -0,0 +1,38 @@
+package com.smarteye.invoke;
+
+import android.graphics.Bitmap;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URLEncoder;
+
+/**
+ * 通用物体和场景识别
+ */
+public class ImageDetectUtil {
+
+    public static final String accessToken = "24.2b4007bbac48d1ea3d941c2cf625666c.2592000.1686816784.282335-33466824";
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    public static String advancedGeneral(Bitmap bitmap) {
+        String url = "https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general?access_token=";
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            String imgStr = Base64Util.encode(byteArrayOutputStream.toByteArray());
+            String imgParam = URLEncoder.encode(imgStr, "UTF-8");
+
+            String param = "image=" + imgParam+"&baike_num=9999";
+            String result = OkHttpClientUtil.doPostXXX(url + accessToken, param);
+
+            System.out.println(result);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
